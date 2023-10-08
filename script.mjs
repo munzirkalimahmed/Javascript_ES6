2907acea103ba2c5bcab37490da212fa10e7b1b3
Munzir Kalim Ahmed <munzir.kalim2@gmail.com>
2023-10-08 17:48:09 +0500

    Initial commit

--- script.mjs
+++ script.mjs
@@ -1,1 +1,13 @@
 console.log("Hello World");
+
+let myNumber = 5;
+let myName = "Kalim"
+
+console.log(myNumber);
+console.log(myName);
+
+myNumber = 100;
+myName = "Munzir";
+
+console.log(myNumber);
+console.log(myName);
