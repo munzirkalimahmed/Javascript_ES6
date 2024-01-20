95a271b25ad3561f6ac1e0f267019fd0dcfec004
Munzir Kalim Ahmed <munzir.kalim2@gmail.com>
2024-01-20 21:23:29 +0500

    initial commit

---
+++ Assignment/test.js
@@ -0,0 +1,27 @@
+
+
+/*class Rectangle{
+    constructor(length,width){
+        this.length=length;
+        this.width=width;
+    }
+    getarea(){
+        document.write(this.length*this.width)
+    }
+    getperimeter(){
+        document.write(2*(this.length+this.width))
+    }
+    display(){
+        document.write(`area of rectangle is ${a.getarea()} and perimeter is ${a.getperimeter()}`)
+    }
+}
+
+console.log("Hello")
+a=new Rectangle(2,3)
+a.display();
+a.getarea()
+a.getperimeter()*/
+
+console.log('Hello')
+
+
